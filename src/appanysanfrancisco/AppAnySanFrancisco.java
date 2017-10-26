ec7bf2ec3d443e3d803ef063e99f66c33898ecaa
any67 <any_sf_1967@hotmail.com>
2017-10-25 21:09:35 -0300

    creando proyecto app

---
+++ src/appanysanfrancisco/AppAnySanFrancisco.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appanysanfrancisco;
+
+/**
+ *
+ * @author LAB08
+ */
+public class AppAnySanFrancisco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    
+    {
+       int numero;
+       float numero2;
+       
+      numero=698;
+      numero2=7.46f;
+      
+      System.out.println("Suma de numero y numero2: "+(numero+numero2));
+      System.out.println("Resta de numero y numero2: "+(numero-numero2));
+      System.out.println("Multiplicacion de numero y numero2: "+(numero*numero2));
+      System.out.println("Division de numero y numero2: "+(numero/numero2));
+      System.out.println("Resto de numero y numero2: "+(numero%numero2));
+        // TODO code application logic here
+    }
+    
+}
